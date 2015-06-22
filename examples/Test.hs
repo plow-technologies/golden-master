46dde46e59492103e9b1590afb3bb842ec08b6b0
Kevin Cotrone <kevincotrone@gmail.com>
2015-06-22 15:04:21 -0500

    Tests with an example OTCTemplate are now serializing and deserializing

--- examples/Test.hs
+++ examples/Test.hs
@@ -1,15 +1,29 @@
 {-# LANGUAGE DataKinds  #-}
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE DeriveGeneric #-}
 
 module Test where
 
 import Data.Master.Template
 import Data.Text
+import Data.Aeson
+import GHC.Generics
+import Control.Monad
 
 data OTCTemplate = OTCTemplate {
   otc :: Template Unnormalized Disjunction Int
 , name :: Template Unnormalized Disjunction Text
-}
+} deriving (Generic)
+
+
+instance ToJSON OTCTemplate where
+  toJSON (OTCTemplate o n) = object ["otc" .= TemplateBox o, "name" .= TemplateBox n]
+instance FromJSON OTCTemplate where
+  parseJSON (Object obj) = do
+    (TemplateBox o) <- obj .: "otc"
+    (TemplateBox nameTemplate) <- obj .: "name"
+    return $ OTCTemplate (Or [FixAny o]) (Or [FixAny nameTemplate])
+  parseJSON _ = mzero
 
 data OTC = OTC {
   otcTest :: Int
@@ -18,7 +32,7 @@ data OTC = OTC {
 
 testOTCTemplate :: OTCTemplate
 testOTCTemplate = OTCTemplate o n
-  where o = Or [FixAny $ And [FixAny $ Lt 100, FixAny $ Gt 10]]
+  where o = Or [FixAny $ And [FixAny $ Lt 100, FixAny $ Gt 10], FixAny $ Not $ FixAny $ Not $ FixAny $ Eq 150 ]
         n = Or [FixAny $ Eq "Test OTC"]
 
 
@@ -26,5 +40,8 @@ checkOTCTemplate :: OTC -> OTCTemplate -> Bool
 checkOTCTemplate (OTC i n) (OTCTemplate otcTemplate nTemplate) = (checkTemplate i otcTemplate) && checkTemplate n nTemplate
 
 
+testTemplateBox :: TemplateBox a -> a -> Bool
+testTemplateBox (TemplateBox t) val = checkTemplate val t
+
 exampleOTC :: OTC
-exampleOTC = OTC 75 "Test OTC"
+exampleOTC = OTC 150 "Test OTC"
